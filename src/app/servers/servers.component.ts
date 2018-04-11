f69e167b7d6c64a8dcd36e933c56bacdfb3170eb
Ross Daniel Kiser <kiserr10@gmail.com>
2018-04-11 11:01:31 -0600

    practice with else and if statements with ngModel

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,8 @@ export class ServersComponent implements OnInit {
 	allowNewServer = false;
 	serverCreationStatus = 'No server was created!'
 	serverName = 'TestServer';
+	serverCreated = false;
+
 	constructor() {
 		setTimeout(() => {
 			this.allowNewServer = true;
@@ -20,7 +22,8 @@ export class ServersComponent implements OnInit {
 	}
 
 	onCreateServer(){
-		this.serverCreationStatus = 'Server was created! The Name of the Server is:' + this.serverName
+		this.serverCreationStatus = 'Server was created! The Name of the Server is:' + this.serverName;
+		this.serverCreated = true;
 	}
 	onUpdateServerName(event: Event){
 		this.serverName = (<HTMLInputElement>event.target).value;
